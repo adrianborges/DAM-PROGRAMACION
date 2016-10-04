b0309e0b01bdaf02b6a198dd2e84327f28eb1051
Adrian <Adrian@192.168.1.113>
2016-10-04 11:08:43 +0200

    Probando string

---
+++ ProbandoString/src/Probandostring.java
@@ -0,0 +1,16 @@
+
+public class Probandostring {
+
+	public static void main(String[] args) {
+		// Definición de variables.
+		String s = new String("Adelante");
+		String s1 = new String ("Vamos adelante");
+		
+		System.out.println(s);
+		System.out.println(s1);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
