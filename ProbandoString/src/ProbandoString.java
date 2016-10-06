25e1cb8fbf136a8bedb9159c4c07c2d044e5db54
Adrian <Adrian@192.168.1.113>
2016-10-06 19:33:50 +0200

    Probando String

---
+++ ProbandoString/src/ProbandoString.java
@@ -0,0 +1,20 @@
+
+public class ProbandoString {
+
+	public static void main(String[] args) {
+		
+		//Definición de variables ( elemento que nos está dafiniendo algo)
+		
+		String s = new String("Estamos");
+		String s1 = new String("Aun Estamos");
+		String s2 = new String("Seguimos Estando");
+		
+		System.out.println(s);
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
